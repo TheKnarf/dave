f5b8429f5faf2138e4aec9573132b34645842c3c
Frank Lyder Bredland <fl@nyggen.com>
2021-04-01 22:09:26 +0200

    Add iconify as a npm dependency

--- src/components/app/index.tsx
+++ src/components/app/index.tsx
@@ -16,17 +16,24 @@ const App : React.FC<AppProps> = ({ id, icon, name, relativeSubdomain, status, u
 	useEffect(() => {
 		if(relativeSubdomain && typeof window !== 'undefined') {
 			setHref(`//${relativeSubdomain}.${window.location.host}/`);
+
 		}
 	}, [relativeSubdomain]);
 
+	useEffect(() => {
+		if(process.browser) {
+			const Iconify = require('@iconify/iconify');
+		}
+	}, [href]);
+
 	return <div className={className}>
 		<a href={href} className={appA}>
-			<span className={`iconify ${appIcon}`} data-icon={`mdi-${icon}`}></span>
-			<div>
-				<div>{name}</div>
-				<div className={appStatus}>{status}</div>
-				<div className={appUrl}>{href}</div>
-			</div>
+		<span className={`iconify ${appIcon}`} data-icon={`mdi-${icon}`}></span>
+		<div>
+			<div>{name}</div>
+			<div className={appStatus}>{status}</div>
+			<div className={appUrl}>{href}</div>
+		</div>
 		</a>
 	</div>;
 }
