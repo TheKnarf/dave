7dc39670f32864774c32cf81ad0c5f020a16594c
Frank Lyder Bredland <fl@nyggen.com>
2021-04-06 13:16:39 +0200

    Adding some default icons. Relates to #7

--- src/fallback-icon/index.tsx
+++ src/fallback-icon/index.tsx
@@ -5,6 +5,18 @@ const fallbackIcon = (dockerImage : string|undefined = undefined) => {
 	switch(dockerImage) {
 		case 'theknarf/hello-world':
 			return 'mdi-earth';
+
+		case 'linuxserver/sonarr':
+			return 'mdi-television-box';
+
+		case 'linuxserver/transmission':
+			return 'mdi-progress-download';
+
+		case 'linuxserver/jackett':
+			return 'mdi-tshirt-crew-outline';
+
+		case 'linuxserver/jellyfin':
+			return 'mdi-plex';
 	};
 
 	return 'mdi-web';
