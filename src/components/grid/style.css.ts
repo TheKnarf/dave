3b32c6cd797bab1b435afcc164b6bc7bad71ad87
Frank Lyder Bredland <fl@nyggen.com>
2021-04-23 19:57:39 +0200

    remove extra semicolon

--- src/components/grid/style.css.ts
+++ src/components/grid/style.css.ts
@@ -1,7 +1,7 @@
 import { style } from '@vanilla-extract/css';
 
 export const className = style({
-	display: 'grid;',
+	display: 'grid',
 	gridTemplateColumns: 'repeat(auto-fill, minmax(540px, 1fr))',
 	rowGap: '1.5em',
 	columnGap: '1.2em',
