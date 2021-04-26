024922f3a11ba0234d65e90db8c8de451dbd6807
Frank Lyder Bredland <fl@nyggen.com>
2021-04-26 12:10:42 +0200

    Reduce Grid min size

--- src/components/grid/style.css.ts
+++ src/components/grid/style.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const className = style({
 	display: 'grid',
-	gridTemplateColumns: 'repeat(auto-fill, minmax(540px, 1fr))',
+	gridTemplateColumns: 'repeat(auto-fill, minmax(330px, 1fr))',
 	rowGap: '1.5em',
 	columnGap: '1.2em',
 });
